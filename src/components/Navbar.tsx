ccbc0e97fbcd382f07c4b3c7b341dba6128947e9
juanfsouza <juanfsouza@gmail.com>
2025-08-15 00:47:42 -0300

    fix: page adm

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -167,7 +167,7 @@ export default function Navbar() {
                       
                       {user?.role === 'admin' && (
                         <DropdownMenuItem asChild className="rounded-lg">
-                          <Link href="/admin/config" className="flex items-center px-3 py-2 cursor-pointer">
+                          <Link href="/mod/config" className="flex items-center px-3 py-2 cursor-pointer">
                             <Settings className="mr-3 h-4 w-4 text-muted-foreground" />
                             <span>Configurações</span>
                           </Link>
